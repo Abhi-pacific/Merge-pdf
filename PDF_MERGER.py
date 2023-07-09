54c35ff7e76a59e037fa5e5c3b8b37d96f62ce42
Pacific <abhishek28c@gmail.com>
2023-07-09 23:58:08 +0530

    voice part done

--- PDF_MERGER.py
+++ PDF_MERGER.py
@@ -1,2 +1,23 @@
-from PyPDF2 import pdfWriter
-import pyt
+from PyPDF2 import PdfWriter
+import pyttsx3
+
+class Pdf_Merge:
+    def __init__(self):
+        self.voice_setup()
+    def voice_setup(self):
+        engine = pyttsx3.init()
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice',voices[1].id)
+        engine.setProperty('rate',150)
+        engine.setProperty('volume',1.0)
+        self.voice_command(engine)
+    def voice_command(self,engine):
+        engine.say('Welcome to PDF merger')
+        engine.say('dont forget to follow Abhishek on Github')
+        print('Welcome to PDF-Merger')
+        engine.runAndWait()
+
+
+def main():
+    pd = Pdf_Merge()
+main()
