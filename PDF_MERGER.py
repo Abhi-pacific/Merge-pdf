ccbac31d93b0d80e08e600ea4f1400bdbb08e5c3
Pacific <abhishek28c@gmail.com>
2023-07-10 11:25:58 +0530

    project completed

--- PDF_MERGER.py
+++ PDF_MERGER.py
@@ -5,6 +5,8 @@ import os
 class Pdf_Merge:
     def __init__(self):
         self.voice_setup()
+
+    # Full setup for the voice
     def voice_setup(self):
         engine = pyttsx3.init()
         voices = engine.getProperty('voices')
@@ -12,27 +14,52 @@ class Pdf_Merge:
         engine.setProperty('rate',150)
         engine.setProperty('volume',1.0)
         self.voice_command(engine)
+    # Welcome voice 
     def voice_command(self,engine):
         engine.say('Welcome to PDF merger')
         engine.say('dont forget to follow Abhishek on Github')
         print('Welcome to PDF-Merger')
-        print(r'format for entering the path D:\\rog_wall\\certificates')
+        engine.say('Please follow below instructions to merge your PDF')
         engine.runAndWait()
-    def merger(self):
+        print('\n\nCreate a folder with any name without space and special characters')
+        print('Paste both the PDF file in that folder ')
+        print('Syntax for the path is :')
+        print(r'(D:\\rog_wall\\certificates)')
+        self.merger(engine)
+    # Merge code start from here
+    def merger(self,engine):
         merge = PdfWriter()
-        print(os.getcwd())
-        path = input('Enter the path of the folder : ')
-        print(path)
-        new_path = os.chdir(path)
-        for file in os.listdir():
-            if file.endswith('.pdf'):
-                merge.append(file)
-        merge.write('merged_pdf.pdf')
-        merge.close()
+        while True:
+            for_exit =int(input('If you want to exit press 1 : '))
+            # using 1 for exit in if condition
+            if for_exit == 1:
+                engine.say('Exiting... thank you for using me')
+                engine.runAndWait()
+                raise SystemExit('Thank you for using me')
+            path = input('Enter the path of the folder : ')
+            engine.say('Your entered path is ')
+            print('Your entered path is :{:^8}'.format(path))
+            try:
+                os.chdir(path)
+                for file in os.listdir():
+                    if file.endswith('.pdf'):
+                        merge.append(file)
+                engine.say('Please Enter name for new file : ')
+                engine.runAndWait()
+                print('Please do not add .pdf in name')
+                new_file_name = input('Please Enter name for new file ')
+                merge.write(new_file_name+'.pdf')
+                merge.close()
+                engine.say('New PDF created, Thanks for using me')
+                engine.runAndWait()
+                raise SystemExit('Thanks you for using me')
+            except FileNotFoundError:
+                engine.say('I believe your entered path is wrong')
+                engine.runAndWait()
+                print('Wrong Path...')
 
 
 
 def main():
     pd = Pdf_Merge()
-    pd.merger()
 main()
