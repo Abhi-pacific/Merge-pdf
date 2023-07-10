201e38f0fb18da771f565a4ef9eb5aadb071d584
Pacific <abhishek28c@gmail.com>
2023-07-10 16:14:30 +0530

    sapi5

--- PDF_MERGER.py
+++ PDF_MERGER.py
@@ -8,7 +8,7 @@ class Pdf_Merge:
 
     # Full setup for the voice
     def voice_setup(self):
-        engine = pyttsx3.init()
+        engine = pyttsx3.init('sapi5')
         voices = engine.getProperty('voices')
         engine.setProperty('voice',voices[1].id)
         engine.setProperty('rate',150)
